6d1823e9503e72dc2e8cb1a978c62bcb13f29033
Mooney <468035416@qq.com>
2017-08-23 20:58:30 +0800

    init

--- app/src/main/java/com/mooney/appinfo/MainActivity.java
+++ app/src/main/java/com/mooney/appinfo/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
-        // 初始化操作
+        // 初始化操作0
         initViews();
         initValues();
         initListeners();
